0ce0d2fa2221ae44e208b58f5ed3dc6dc4a88298
noimeta <noi.meta@gmail.com>
2019-11-01 21:04:21 +0700

    add my name to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -43,12 +43,10 @@ const AboutPage = () => (
           <div>
             <ExternalLink href="https://github.com/rapee">
               Rapee Suveeranont
-            </ExternalLink>
+            </ExternalLink>{" "}
             <ExternalLink href="https://th1nkk1d.xyz">
               Withee Poositasai
-            </ExternalLink>
-          </div>
-          <div>
+            </ExternalLink>{" "}
             <ExternalLink href="https://github.com/noimeta">
               Noi Meta 😼
             </ExternalLink>
