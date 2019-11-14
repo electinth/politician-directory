3930e41dd1f12296abe1ffa8a4888a50b39f59ef
Tanut Treratanakulwong <tanat44@gmail.com>
2019-11-14 11:19:55 +0700

    Draft PR #71 cabinet page with responsive

--- src/pages/cabinet.js
+++ src/pages/cabinet.js
@@ -6,6 +6,9 @@ import SEO from "../components/seo"
 import StackedBarChart from "../components/stackedBarChart"
 import { graphql } from "gatsby"
 import moment from "moment"
+import PeopleCardMini from "../components/peopleCardMini";
+import './cabinet.css'
+import { calculateBackground, combineCategory, padCategory, birthdayToAgeHistogram } from "../utils"
 
 export const query = graphql`
   query {
@@ -34,49 +37,24 @@ export const query = graphql`
         }
       }
     }
+    keyMembers: allPeopleYaml(filter: {id: {in: ["13","15","35","235"]}}) {
+      edges {
+        node {
+          id
+          title
+          name
+          lastname
+          party
+          party_group
+          mp_type
+          mp_province
+          mp_zone
+          mp_list
+        }
+      }
+    }
   }
 `
-
-// Mock for StackedBarChart
-const mpTypes = [
-  { name: "ส.ส. บัญชีรายชื่อ", background: "#ffffff", value: 315 },
-  { name: "ส.ส. เขต", background: "#000000", value: 185 },
-]
-
-const genders = [
-  { name: "เพศหญิง", background: "#ffffff", value: 125 },
-  { name: "เพศชาย", background: "#000000", value: 375 },
-]
-
-const ages = [
-  { name: "5 - 24 ปี", background: "#eeeeee", value: 71 },
-  { name: "25 - 38 ปี", background: "#d5d1d1", value: 181 },
-  { name: "39 - 73 ปี", background: "#9a9292", value: 228 },
-  { name: "73 - 91 ปี", background: "#222222", value: 10 },
-]
-
-const cssSection = {
-  paddingTop: "8rem",
-  paddingBottom: "5rem",
-  h2: {
-    fontSize: "4.8rem",
-    textAlign: "center",
-  },
-  width: "100%",
-}
-
-const cssSectionWhite = {
-  ...cssSection,
-  background: "var(--cl-white)",
-}
-
-const cssPage = {
-  width: "50%",
-  height: "100%",
-  display: "inline-block",
-  padding: "0 2rem 0 2rem",
-  verticalAlign: "top",
-}
 
 const cssH1 = {
   fontSize: "4.8rem",
@@ -85,7 +63,7 @@ const cssH1 = {
 const cssEngTitle = {
   fontSize: "2.4rem",
   textAlign: "left",
-  margin: "3rem 0 2rem 0",
+  margin: "1.5rem 0 1.2rem 0",
 }
 
 const cssPageP = {
@@ -110,50 +88,8 @@ const cssLinkBox = {
   },
 }
 
-function float2Grey(x) {
-  x = 1 - x
-  x = parseInt(x * 15)
-  x = x.toString(16)
-  return "#" + x + x + x
-}
-
-function calculateBackground(input) {
-  let count = input.length - 1
-  input.map((x, idx) => {
-    x.background = float2Grey(idx / count)
-  })
-  return input
-}
-
-function combineCategory(input) {
-  input.sort((a, b) => parseInt(b.value) - parseInt(a.value))
-  let temp = []
-  let count_others = 0
-  input.map((x, idx) => {
-    if (idx > 2) {
-      count_others += x.value
-    } else {
-      temp.push(x)
-    }
-    return x
-  })
-  temp[temp.length] = { value: count_others, name: "อื่นๆ" }
-  input = temp
-  return input
-}
-
-function padCategory(input) {
-  input.map(x => {
-    if (x.name.length === 0) {
-      x.name = "ไม่พบข้อมูล"
-    }
-    return x
-  })
-  return input
-}
-
 const CabinetPage = data => {
-  console.log(data)
+
   data = data.data
   let education = data.education.group
   education = calculateBackground(education)
@@ -166,62 +102,29 @@ const CabinetPage = data => {
   let gender = data.gender.group
   gender = calculateBackground(gender)
 
-  let _age = data.age.edges
+  let birthdate = data.age.edges
   const ageBin = [39, 52, 65]
-  let age = []
-  age.push({ name: String("25-" + String(ageBin[0] - 1)) + " ปี", value: 0 })
-  age.push({
-    name: String(ageBin[0]) + "-" + String(ageBin[1] - 1) + " ปี",
-    value: 0,
-  })
-  age.push({
-    name: String(ageBin[1]) + "-" + String(ageBin[2] - 1) + " ปี",
-    value: 0,
-  })
-  age.push({ name: String(">" + ageBin[2]) + " ปี", value: 0 })
-  const today = parseInt(moment().format("YYYY"))
-  _age.map(x => {
-    const y = parseInt(moment(x.node.birthdate).format("YYYY"))
-    // let temp = (today - d) / 1000 / 3600 / 24
-    // x.value = parseInt(temp)
-    const a = today - y
-    if (a < ageBin[0]) {
-      age[0].value++
-    } else if (a < ageBin[1]) {
-      age[1].value++
-    } else if (a < ageBin[2]) {
-      age[2].value++
-    } else {
-      age[3].value++
-    }
+  let age = birthdayToAgeHistogram(birthdate, ageBin)
+  age = calculateBackground(age)
+
+  let keyMembers = data.keyMembers.edges
+  let keyPosition = ['ประธาน', 'รองประธานคนที่ 1', 'รองประธานคนที่ 2', 'ผู้นำฝ่ายค้าน']
+  let k = []
+  keyMembers.map( (x, idx) => {
+    let aPerson = x.node
+    aPerson.pos = keyPosition[idx]
+    k.push(aPerson)
     return x
   })
-
-  age = calculateBackground(age)
+  keyMembers = k
 
   return (
     <Layout pageStyles={{ background: "#C0E4FF" }}>
       <SEO title="คณะรัฐมนตรี" />
 
-      <section css={{ ...cssSection }}>
-        <div
-          style={{
-            width: "962px",
-            height: "665px",
-            margin: "auto",
-            backgroundColor: "var(--cl-white)",
-            borderRadius: "1rem",
-            boxShadow: "5px 5px var(--cl-black)",
-          }}
-        >
-          <div
-            style={{
-              ...cssPage,
-              borderRadius: "1rem 0 0 1rem",
-              background:
-                "linear-gradient(90deg, rgba(255,255,255,1) 72%, rgba(246,246,246,1) 100%)",
-            }}
-          >
+      <section className="section">
+        <div className="book">
+          <div className="page leftPage">
             <h1 css={{ ...cssH1, margin: "1rem 0 0 0" }}>คณะรัฐมนตรีไทย</h1>
             <h2 style={{ ...cssEngTitle }}>Cabinet</h2>
             <h2 style={{ ...cssEngTitle }}>About</h2>
@@ -246,8 +149,16 @@ const CabinetPage = data => {
             <h2 style={{ ...cssEngTitle }}>Since</h2>
             <p>01.04.2554</p>
             <h2 style={{ ...cssEngTitle }}>Key Members</h2>
+            {keyMembers.map(x => {
+              return (
+                <div className="peopleCard" key={x.id}>
+                  <PeopleCardMini key={x.id} {...x}/>
+                </div>
+              )
+            })}
+            
           </div>
-          <div style={{ ...cssPage }}>
+          <div className="page">
             <h2
               style={{
                 ...cssEngTitle,
@@ -282,14 +193,12 @@ const CabinetPage = data => {
             </div>
           </div>
         </div>
-        <h2
-          style={{ fontSize: "2.4rem", textAlign: "center", marginTop: "6rem" }}
-        >
+        <h2 className="lastUpdate">
           Last Update: 30.10.2019
         </h2>
       </section>
 
-      <section css={{ ...cssSectionWhite }}></section>
+      <section className="section" css={{ background: "var(--cl-white)" }}></section>
     </Layout>
   )
 }
