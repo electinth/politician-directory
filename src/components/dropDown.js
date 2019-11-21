6dc619be00572426910144eb279b454be5a6ebfa
thasarito <thasaritp@icloud.com>
2019-11-21 23:07:07 +0700

    refactor dropdown to its own component and click outside now close the dropdown

---
+++ src/components/dropDown.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react"
+
+import { css, Global } from "@emotion/core"
+
+class DropDown extends Component {
+  container = React.createRef()
+
+  state = {
+    show: false,
+  }
+
+  handleFilter = (e, field) => {
+    this.setState({ show: !this.state.show })
+    this.props.handleFilter(e, field)
+  }
+  handleClickOutside = e => {
+    if (this.container.current && !this.container.current.contains(e.target)) {
+      this.setState({
+        show: false,
+      })
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside)
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside)
+  }
+  render() {
+    return (
+      <li ref={this.container}>
+        <button
+          onClick={() => this.setState({ show: !this.state.show })}
+          className="currentFilter"
+        >
+          {this.props.currentFilter[this.props.filter]}
+          <span>
+            <i className="arrow-down"></i>
+          </span>
+        </button>
+        {this.state.show ? (
+          <div className="menuItems">
+            <button onClick={e => this.handleFilter(e, this.props.filter)}>
+              {this.props.choices.default}
+            </button>
+            {this.props.choices.others.map(choice => (
+              <button onClick={e => this.handleFilter(e, this.props.filter)}>
+                {choice}
+              </button>
+            ))}
+          </div>
+        ) : null}
+      </li>
+    )
+  }
+}
+
+export default ({ choices, currentFilter, handleFilter }) => {
+  return (
+    <ul className="current-filter-list">
+      <Global
+        styles={css`
+          .arrow-down {
+            margin-top: 5px;
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-top: 10px solid var(--cl-black);
+            float: right;
+          }
+
+          .current-filter-list {
+            text-align: center;
+            list-style: none;
+            button {
+              text-align: left;
+              background: var(--cl-pink);
+              border: 1px var(--cl-black) solid;
+              cursor: pointer;
+              width: 165px;
+              height: 40px;
+              margin: 1px 7.5px;
+            }
+            .currentFilter {
+              border-radius: 7.5px;
+            }
+            button:focus {
+              outline: none;
+            }
+            li {
+              display: inline-block;
+              position: relative;
+            }
+          }
+
+          .menuItems {
+            position: absolute;
+            z-index: 1;
+            border: 1px solid var(--cl-black);
+            width: 165px;
+            margin: 1px 7.5px;
+            button {
+              display: block;
+              border: none;
+              background-color: white;
+              margin: 0px;
+              width: 100%;
+            }
+            button:hover {
+              background-color: var(--cl-pink);
+            }
+          }
+        `}
+      />
+      {Object.entries(choices).map(([filter, choices]) => (
+        <DropDown
+          currentFilter={currentFilter}
+          handleFilter={handleFilter}
+          filter={filter}
+          choices={choices}
+        />
+      ))}
+    </ul>
+  )
+}
--- src/components/waffleFilter.js
+++ src/components/waffleFilter.js
@@ -2,6 +2,9 @@ import React, { Component } from "react"
 
 import { css, Global } from "@emotion/core"
 
+import _ from "lodash"
+
+import DropDown from "../components/dropDown"
 import Waffle from "../components/waffle"
 
 const cssH1 = { fontSize: "4.8rem", marginTop: "4rem" }
@@ -23,37 +26,10 @@ class WaffleFilter extends Component {
     house: this.props.data,
     data_of_interest: [],
     data_the_rest: [],
-    showMenu: null,
-    currentFilter: {
-      house: "ทั้งหมด",
-      gender: "ทุกเพศ",
-      generation: "ทุกช่วงวัย",
-      education: "การศึกษาทั้งหมด",
-      occupation_group: "ทุกกลุ่มอาชีพ (เดิม)",
-    },
-  }
-
-  handleHouse = e => {
-    let currentFilter = { ...this.state.currentFilter }
-    let filter = e.target.innerText
-    currentFilter["house"] = filter
-    let house = [...this.props.data]
-    if (filter !== "ทั้งหมด") {
-      house = house.filter(
-        x => x.node["house"] === filter && x.node["is_active"] === true
-      )
-    }
-    let [data_of_interest, data_the_rest] = this.filterData(
-      house,
-      currentFilter
-    )
-    this.setState({
-      showMenu: null,
-      currentFilter,
-      house,
-      data_of_interest,
-      data_the_rest,
-    })
+    currentFilter: _.zipObject(
+      Object.keys(this.props.choices),
+      Object.values(this.props.choices).map(choice => choice.default)
+    ),
   }
 
   filterData = (data, filter) => {
@@ -65,13 +41,9 @@ class WaffleFilter extends Component {
         for (var key in filter) {
           if (
             /// if default filter then don't change interest
-            ![
-              "ทั้งหมด",
-              "ทุกเพศ",
-              "ทุกช่วงวัย",
-              "การศึกษาทั้งหมด",
-              "ทุกกลุ่มอาชีพ (เดิม)",
-            ].includes(filter[key])
+            !Object.values(this.props.choices)
+              .map(choice => choice.default)
+              .includes(filter[key])
           ) {
             interested = interested && person.node[key] === filter[key]
           }
@@ -87,85 +59,48 @@ class WaffleFilter extends Component {
   }
 
   handleFilter = (e, field) => {
-    let currentFilter = { ...this.state.currentFilter }
-    currentFilter[field] = e.target.innerText
-    let [data_of_interest, data_the_rest] = this.filterData(
-      this.state.house,
-      currentFilter
-    )
-    this.setState({
-      showMenu: null,
-      currentFilter,
-      data_of_interest,
-      data_the_rest,
-    })
+    if (field === "house") {
+      let currentFilter = { ...this.state.currentFilter }
+      let filter = e.target.innerText
+      currentFilter["house"] = filter
+      let house = [...this.props.data]
+      if (filter !== "ทั้งหมด") {
+        house = house.filter(
+          x => x.node["house"] === filter && x.node["is_active"] === true
+        )
+      }
+      let [data_of_interest, data_the_rest] = this.filterData(
+        house,
+        currentFilter
+      )
+      this.setState({
+        currentFilter,
+        house,
+        data_of_interest,
+        data_the_rest,
+      })
+    } else {
+      let currentFilter = { ...this.state.currentFilter }
+      currentFilter[field] = e.target.innerText
+      let [data_of_interest, data_the_rest] = this.filterData(
+        this.state.house,
+        currentFilter
+      )
+      this.setState({
+        currentFilter,
+        data_of_interest,
+        data_the_rest,
+      })
+    }
   }
-
-  uniqueChoices = group => [...new Set(this.props.data.map(x => x.node[group]))]
 
   componentDidMount() {
     this.handleFilter({ target: { innerText: "หญิง" } }, "gender")
   }
 
   render() {
-    console.log(this.state.data_of_interest)
     return (
       <>
-        <Global
-          styles={css`
-            .arrow-down {
-              margin-top: 5px;
-              width: 0;
-              height: 0;
-              border-left: 5px solid transparent;
-              border-right: 5px solid transparent;
-              border-top: 10px solid var(--cl-black);
-              float: right;
-            }
-
-            .current-filter-list {
-              text-align: center;
-              list-style: none;
-              button {
-                text-align: left;
-                background: var(--cl-pink);
-                border: 1px var(--cl-black) solid;
-                cursor: pointer;
-                width: 165px;
-                height: 40px;
-                margin: 1px 7.5px;
-              }
-              .currentFilter {
-                border-radius: 7.5px;
-              }
-              button:focus {
-                outline: none;
-              }
-              li {
-                display: inline-block;
-                position: relative;
-              }
-            }
-
-            .menuItems {
-              position: absolute;
-              z-index: 1;
-              border: 1px solid var(--cl-black);
-              width: 165px;
-              margin: 1px 7.5px;
-              button {
-                display: block;
-                border: none;
-                background-color: white;
-                margin: 0px;
-                width: 100%;
-              }
-              button:hover {
-                background-color: var(--cl-pink);
-              }
-            }
-          `}
-        />
         <h2 css={{ ...cssH1 }}>สัดส่วนผู้แทนของเรา พวกเขาเป็นใครบ้าง</h2>
         <h2>
           <span css={{ fontSize: "7.2rem", verticalAlign: "middle" }}>
@@ -185,7 +120,7 @@ class WaffleFilter extends Component {
                 ? ""
                 : "เกิดในยุค " + this.state.currentFilter.generation
             } ${
-              this.state.currentFilter.education === "การศึกษาทั้งหมด"
+              this.state.currentFilter.education === "ทุกระดับการศึกษา"
                 ? ""
                 : (this.state.currentFilter.education === "สถาบันทหาร"
                     ? "จบการศึกษาจาก"
@@ -199,123 +134,11 @@ class WaffleFilter extends Component {
           </span>
         </h2>
         <div css={{ margin: "50px auto 0 auto" }}>
-          <ul className="current-filter-list">
-            <li>
-              <button
-                onClick={() => this.setState({ showMenu: 0 })}
-                className="currentFilter"
-              >
-                {this.state.currentFilter.house}
-                <span>
-                  <i className="arrow-down"></i>
-                </span>
-              </button>
-              {this.state.showMenu === 0 ? (
-                <div className="menuItems">
-                  <button onClick={e => this.handleHouse(e)}>ทั้งหมด</button>
-                  {this.uniqueChoices("house").map(choice => (
-                    <button onClick={e => this.handleHouse(e)}>{choice}</button>
-                  ))}
-                </div>
-              ) : null}
-            </li>
-            <li>
-              <button
-                onClick={() => this.setState({ showMenu: 1 })}
-                className="currentFilter"
-              >
-                {this.state.currentFilter.gender}
-                <span>
-                  <i className="arrow-down"></i>
-                </span>
-              </button>
-              {this.state.showMenu === 1 ? (
-                <div className="menuItems">
-                  <button onClick={e => this.handleFilter(e, "gender")}>
-                    ทุกเพศ
-                  </button>
-                  {this.uniqueChoices("gender").map(choice => (
-                    <button onClick={e => this.handleFilter(e, "gender")}>
-                      {choice}
-                    </button>
-                  ))}
-                </div>
-              ) : null}
-            </li>
-            <li>
-              <button
-                onClick={() => this.setState({ showMenu: 2 })}
-                className="currentFilter"
-              >
-                {this.state.currentFilter.generation}
-                <span>
-                  <i className="arrow-down"></i>
-                </span>
-              </button>
-              {this.state.showMenu === 2 ? (
-                <div className="menuItems">
-                  <button onClick={e => this.handleFilter(e, "generation")}>
-                    ทุกช่วงวัย
-                  </button>
-                  {this.uniqueChoices("generation").map(choice => (
-                    <button onClick={e => this.handleFilter(e, "generation")}>
-                      {choice}
-                    </button>
-                  ))}
-                </div>
-              ) : null}
-            </li>
-            <li>
-              <button
-                onClick={() => this.setState({ showMenu: 3 })}
-                className="currentFilter"
-              >
-                {this.state.currentFilter.education}
-                <span>
-                  <i className="arrow-down"></i>
-                </span>
-              </button>
-              {this.state.showMenu === 3 ? (
-                <div className="menuItems">
-                  <button onClick={e => this.handleFilter(e, "education")}>
-                    การศึกษาทั้งหมด
-                  </button>
-                  {this.uniqueChoices("education").map(choice => (
-                    <button onClick={e => this.handleFilter(e, "education")}>
-                      {choice}
-                    </button>
-                  ))}
-                </div>
-              ) : null}
-            </li>
-            <li>
-              <button
-                onClick={() => this.setState({ showMenu: 4 })}
-                className="currentFilter"
-              >
-                {this.state.currentFilter.occupation_group}
-                <span>
-                  <i className="arrow-down"></i>
-                </span>
-              </button>
-              {this.state.showMenu === 4 ? (
-                <div className="menuItems">
-                  <button
-                    onClick={e => this.handleFilter(e, "occupation_group")}
-                  >
-                    ทุกกลุ่มอาชีพ (เดิม)
-                  </button>
-                  {this.uniqueChoices("occupation_group").map(choice => (
-                    <button
-                      onClick={e => this.handleFilter(e, "occupation_group")}
-                    >
-                      {choice}
-                    </button>
-                  ))}
-                </div>
-              ) : null}
-            </li>
-          </ul>
+          <DropDown
+            choices={this.props.choices}
+            currentFilter={this.state.currentFilter}
+            handleFilter={this.handleFilter}
+          />
           <Waffle
             data={[this.state.data_of_interest, this.state.data_the_rest]}
             colors={[`var(--cl-pink)`, `var(--cl-gray-3)`]}
@@ -350,6 +173,20 @@ export default ({ data }) => {
       person.node.house = "ไม่ใช่ ส.ส. หรือ ส.ว."
     }
   })
+  const uniqueChoices = group => [...new Set(data.map(x => x.node[group]))]
+  const choices = {
+    house: { default: "ทั้งหมด", others: uniqueChoices("house") },
+    gender: { default: "ทุกเพศ", others: uniqueChoices("gender") },
+    generation: { default: "ทุกช่วงวัย", others: uniqueChoices("generation") },
+    education: {
+      default: "ทุกระดับการศึกษา",
+      others: uniqueChoices("education"),
+    },
+    occupation_group: {
+      default: "ทุกกลุ่มอาชีพ (เดิม)",
+      others: uniqueChoices("occupation_group"),
+    },
+  }
 
-  return <WaffleFilter data={data} />
+  return <WaffleFilter data={data} choices={choices} />
 }
