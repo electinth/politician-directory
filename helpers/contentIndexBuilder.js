4aeb2ec68c572a5b8de2a7f0870f26538d031e05
Th1nkK1D <witheep@gmail.com>
2019-11-26 22:09:51 +0700

    Index builder support config with multiple content

--- helpers/contentIndexBuilder.js
+++ helpers/contentIndexBuilder.js
@@ -1,14 +1,21 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-try {
-  const doc = yaml.safeLoad(fs.readFileSync('../src/contents/party.yaml', 'utf8')).map(rec => ({
-      q: rec.name,
-      type: 'party',
-      page: `/party/${rec.name}`,
-  }))
+const config = [
+  { fileName: 'party.yaml', getQ: record => record.name, type: 'party', getPage: record => `/party/${record.name}` },
+]
 
-  fs.writeFileSync('../src/contents/index.yaml', yaml.safeDump(doc), 'utf8')
+try {
+  const contentIndex = config.reduce((prevContent, content) => [
+    ...prevContent,
+    ...yaml.safeLoad(fs.readFileSync(`../src/contents/${content.fileName}`, 'utf8')).map(record => ({
+      q: content.getQ(record),
+      type: content.type,
+      page: content.getPage(record),
+    }))
+  ], [])
+
+  fs.writeFileSync('../src/contents/index.yaml', yaml.safeDump(contentIndex), 'utf8')
 } catch (e) {
   console.log(e)
 }
