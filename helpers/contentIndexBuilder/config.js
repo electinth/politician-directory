a396c374c2a05f9bf15b6f7418e78e937cab0157
Th1nkK1D <witheep@gmail.com>
2019-11-26 22:29:57 +0700

    Split config and restructure file

---
+++ helpers/contentIndexBuilder/config.js
@@ -0,0 +1,19 @@
+module.exports = [
+    { 
+        type: 'party',
+        path: './src/contents/party.yaml',
+        getQ: record => record.name,
+        getUrl: record => `/party/${record.name}`
+    },
+    {
+        type: 'people',
+        path: './src/contents/people.yaml',
+        getQ: record => `${record.title} ${record.name} ${record.lastname}`,
+        getUrl: record => `/people/${record.name}-${record.lastName}` },
+    { 
+        type: 'votelog',
+        path: './src/contents/votelog.yaml',
+        getQ: record => record.title,
+        getUrl: record => `/votelog/${record.id}`
+    },
+]
