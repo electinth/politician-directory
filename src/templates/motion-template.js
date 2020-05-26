1f7653787de4e66b71ffbfdee9c2c93894678c17
thasarito <thasarit.pysr@gmail.com>
2020-05-26 21:46:46 +0700

    query motion data

--- src/templates/motion-template.js
+++ src/templates/motion-template.js
@@ -3,28 +3,44 @@ import { graphql } from "gatsby"
 
 export const query = graphql`
   query($id: String!) {
-    allMotionYaml(filter: { id: { eq: $id } }) {
-      nodes {
-        id
+    motion: motionYaml(id: { glob: $id }) {
+      id
+      content
+      name
+      votelog_id
+      vote_url
+      purposers {
+        last_name
         name
-        page_url
+        party
+        title
       }
+      seconders {
+        last_name
+        name
+        party
+        title
+      }
+      select_committee
+      status
+      sub_cat
+      vote_date(formatString: "")
+      vote_result
+      registration_no
+      proposal_date(formatString: "")
+      page_url
+      main_cat
     }
   }
 `
 
 const MotionPage = props => {
   const {
-    pageContext: { id },
-    data: {
-      allMotionYaml: {
-        nodes: [motion],
-      },
-    },
+    data: { motion },
   } = props
 
-  console.log(motion)
-  return <h1>Motion Detail Pages {motion.id}</h1>
+  console.log(props)
+  return <h1>Motion Detail Pages {motion.name}</h1>
 }
 
 export default MotionPage
