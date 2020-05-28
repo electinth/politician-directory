1c185de1d439d9aeb1cb28c7d771acf279c84636
thasarito <thasarit.pysr@gmail.com>
2020-05-28 17:39:25 +0700

    style nominator profile

--- src/components/motion/nominator.js
+++ src/components/motion/nominator.js
@@ -2,22 +2,60 @@ import React from "react"
 
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
+import { politicianPicture } from "../../utils"
+
+const ProfilePic = ({ name, last_name }) => {
+  const profile = { name, lastname: last_name }
+  return <img src={politicianPicture(profile)} alt="" />
+}
+
+export { ProfilePic }
+
+const ProfileContainer = styled.li`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.08rem;
+
+  & .profile-pic {
+    width: 40px;
+    height: 40px;
+
+    clip-path: circle(50%);
+    margin-right: 15px;
+  }
+`
+
+const profile = ({ name, last_name, party }) => {
+  return (
+    <ProfileContainer>
+      <div className="profile-pic">
+        <ProfilePic name={name} last_name={last_name} />
+      </div>
+      <div className="detail">
+        <div className="name">
+          {name} {last_name}
+        </div>
+        <div className="party">{party}</div>
+      </div>
+    </ProfileContainer>
+  )
+}
+const Profile = styled(profile)``
 
 const nominator = ({ className, motion: { purposers, seconders } }) => {
   return (
     <ul className={className}>
       <li>
-        ผู้เสนอ
+        <h4>ผู้เสนอ</h4>
         <ul>
-          {purposers.map(({ title, name, last_name, party }) => (
-            <li key={name + last_name}>
-              {name} {last_name}
-            </li>
+          {purposers.map(id => (
+            <Profile key={id.name + id.last_name} {...id} />
           ))}
         </ul>
       </li>
       <li>
-        ผู้รับรอง
+        <h4>ผู้รับรอง</h4>
         <ul
           css={css`
             /* height: 50vh;
@@ -29,13 +67,8 @@ const nominator = ({ className, motion: { purposers, seconders } }) => {
             }
           `}
         >
-          {seconders.map(({ title, name, last_name, party }) => (
-            <li key={name + last_name}>
-              <div>
-                {name} {last_name}
-              </div>
-              <div className="party">{party}</div>
-            </li>
+          {seconders.map(id => (
+            <Profile key={id.name + id.last_name} {...id} />
           ))}
         </ul>
       </li>
@@ -45,6 +78,14 @@ const nominator = ({ className, motion: { purposers, seconders } }) => {
 const Nominator = styled(nominator)`
   height: 80vh;
   overflow-y: scroll;
+  list-style: none;
+  padding: 10px 20px;
+
+  & > li > h4 {
+    margin: 20px 0;
+    font-size: 22px;
+    color: var(--cl-gray-0);
+  }
 `
 
 export default Nominator
