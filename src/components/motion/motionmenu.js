60991a059825719b6a6eb7b27eda39cca2fb1995
thasarito <thasarit.pysr@gmail.com>
2020-05-30 10:31:05 +0700

    style motionmenu

--- src/components/motion/motionmenu.js
+++ src/components/motion/motionmenu.js
@@ -8,12 +8,34 @@ import { useRef } from "react"
 
 const MotionList = styled.ul`
   list-style: none;
-  height: 60vh;
+  height: 80vh;
   overflow-y: auto;
+
+  /* hiding scrollbar */
+  width: 100%;
+  padding-right: 17px;
+  box-sizing: content-box;
+  margin: 0;
 `
 
 const Motion = styled.li`
-  background-color: ${({ active }) => active && "rgba(240, 240, 240, 1)"};
+  background-color: ${({ active }) => active && "var(--cl-gray-4)"};
+  opacity: ${({ active }) => (active ? 1 : 0.5)};
+  padding: 22.5px 20px;
+  margin: 0;
+
+  box-sizing: border-box;
+  width: 300px;
+
+  & a {
+    color: black;
+    text-decoration: none;
+  }
+
+  &:hover {
+    background-color: var(--cl-gray-4);
+    opacity: 1;
+  }
 `
 
 const Motionmenu = ({ name, motionCat, className }) => {
@@ -28,8 +50,14 @@ const Motionmenu = ({ name, motionCat, className }) => {
   }, [listRef])
 
   return (
-    <div className={className}>
-      MotionList
+    <div
+      className={className}
+      css={css`
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+      `}
+    >
       <MotionList ref={listRef}>
         {motionCat.map(motion => (
           <Motion
@@ -38,7 +66,13 @@ const Motionmenu = ({ name, motionCat, className }) => {
             active={motion.name === name}
           >
             <Link to={motion.fields.slug}>
-              <div>{motion.registration_no}</div>
+              <div
+                css={css`
+                  margin-bottom: 5px;
+                `}
+              >
+                {motion.registration_no}
+              </div>
               <div>{motion.name}</div>
             </Link>
           </Motion>
