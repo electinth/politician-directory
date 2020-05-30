65b7d264876b2db26c2b49b30bdbf1ef29c2a1f7
thasarito <thasarit.pysr@gmail.com>
2020-05-30 10:23:07 +0700

    default to scroll to current motion

--- src/components/motion/motionmenu.js
+++ src/components/motion/motionmenu.js
@@ -3,29 +3,50 @@ import React from "react"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import { Link } from "gatsby"
+import { useEffect } from "react"
+import { useRef } from "react"
 
 const MotionList = styled.ul`
   list-style: none;
   height: 60vh;
   overflow-y: auto;
 `
-const motionmenu = ({ name, motionCat, className }) => {
+
+const Motion = styled.li`
+  background-color: ${({ active }) => active && "rgba(240, 240, 240, 1)"};
+`
+
+const Motionmenu = ({ name, motionCat, className }) => {
+  const listRef = useRef(null)
+  useEffect(() => {
+    if (!listRef) return
+    // when component did mount
+    // set scroll to current motion
+    const currentMotion = document.getElementById("current-motion")
+    listRef.current.scrollTop =
+      currentMotion.offsetTop - currentMotion.offsetHeight
+  }, [listRef])
+
   return (
     <div className={className}>
       MotionList
-      <MotionList>
+      <MotionList ref={listRef}>
         {motionCat.map(motion => (
-          <li key={motion.name}>
+          <Motion
+            key={motion.name}
+            id={motion.name === name ? "current-motion" : ""}
+            active={motion.name === name}
+          >
             <Link to={motion.fields.slug}>
               <div>{motion.registration_no}</div>
               <div>{motion.name}</div>
             </Link>
-          </li>
+          </Motion>
         ))}
       </MotionList>
     </div>
   )
 }
-const MotionMenu = styled(motionmenu)``
+const MotionMenu = styled(Motionmenu)``
 
 export default MotionMenu
