625348998e90298c38f3be1aa4d8ea28374edf3d
mixth <me@mixth.net>
2020-05-31 15:40:47 +0700

    AAdjust some css attributes

--- src/components/floatingCard.js
+++ src/components/floatingCard.js
@@ -5,7 +5,7 @@ const FloatingCard = ({ children, ...props }) => {
   return (
     <div
       css={{
-        boxShadow: "0 0 16px 0 #F4F4F4",
+        boxShadow: "0 0 16px 16px #F4F4F4",
         borderRadius: "1rem",
         backgroundColor: "var(--cl-white)",
       }}
