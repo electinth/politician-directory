f8368d79e840555af03345eea145f92062e9b570
thasarito <thasarit.pysr@gmail.com>
2020-06-13 22:37:47 +0700

    adjust bar height

--- src/components/motion/statusBarChart.js
+++ src/components/motion/statusBarChart.js
@@ -37,7 +37,7 @@ const StatusBarChart = ({ data, width }) => {
         className="statusbar-container"
         css={css`
           width: ${width};
-          height: 200px;
+          height: 100px;
           background-color: white;
 
           display: flex;
