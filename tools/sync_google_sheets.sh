6a019eaa22ec3667049198dc4dbd563188426185
Rapee Suveeranont <iiieyes@gmail.com>
2020-07-02 09:34:01 +0700

    Update data

--- tools/sync_google_sheets.sh
+++ tools/sync_google_sheets.sh
@@ -7,43 +7,43 @@ mkdir -p tmp/
 
 # People
 echo "people"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1739107794&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1739107794&single=true&output=csv' \
   | sed '1d' > tmp/people.csv
 node -r esm csv2yaml tmp/people.csv >> build.log
 
 # Party
 echo "party"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1527118532&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1527118532&single=true&output=csv' \
   | sed '1,3d' > tmp/party.csv
 node -r esm csv2yaml tmp/party.csv >> build.log
 
 # Votelog
 echo "votelog"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1873669969&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1873669969&single=true&output=csv' \
   | sed '1d' > tmp/votelog.csv
 node -r esm csv2yaml tmp/votelog.csv >> build.log
 
 # PeopleVotelog
 echo "people_vote"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=435210633&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=435210633&single=true&output=csv' \
   | sed '1d' > tmp/people_vote.csv
 node -r esm csv2yaml tmp/people_vote.csv >> build.log
 
 # Motion
 echo "motion"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=1593490955&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=1593490955&single=true&output=csv' \
   | sed '1d' > tmp/motion.csv
 node -r esm csv2yaml tmp/motion.csv >> build.log
 
 # Motion Categories
 echo "motion_cat"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=877287671&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=877287671&single=true&output=csv' \
   > tmp/motion_cat.csv
 node -r esm csv2yaml tmp/motion_cat.csv >> build.log
 
 # Translations
 echo "translation"
-curl -s 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=348578468&single=true&output=csv' \
+curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=348578468&single=true&output=csv' \
   | sed '1d' > tmp/translation.csv
 node -r esm csv2yaml tmp/translation.csv >> build.log
 
