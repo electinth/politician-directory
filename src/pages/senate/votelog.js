26b820c523f9a3d34bd79aa9c5a986bf199ada9f
key <naanong@punchup.world>
2020-07-23 21:19:29 +0700

    Create vote log pop up and vote log header component

--- src/pages/senate/votelog.js
+++ src/pages/senate/votelog.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { graphql } from "gatsby"
 import _ from "lodash"
 import Layout from "../../components/layout"
+import VotelogHeader from "../../components/votelogHeader"
+import VoteInfoPopup from "../../components/voteInfoPopup"
 import Autocomplete from "../../components/autocomplete"
+import MockBarchart from "../../components/mockBarchart"
 
 export const query = graphql`
   query {
@@ -49,19 +52,35 @@ export const query = graphql`
 `
 
 const VotelogPage = ({ data }) => {
+  const voteSelected = _.find(data.allSenateVotelogYaml.nodes, { 'id': '1' })
   const [senatorId, setSenatorId] = useState('0');
+  const [popupState, setPopupState] = useState(false);
   const [viewPerson, setViewPerson] = useState(true);
   const [viewGroup, setViewGroup] = useState(false);
   
   return (
     <div>
+      <VoteInfoPopup 
+        popupState={popupState}
+        setPopupState={setPopupState}
+        votelogInfo={voteSelected}
+        allSenateVoteYaml={data.allSenateVoteYaml}
+        allPeopleYaml={data.allPeopleYaml}
+      />
       <Layout style={{position: "relative"}}>
+        <VotelogHeader 
+          viewPerson={viewPerson} 
+          viewGroup={viewGroup} 
+          setViewPerson={setViewPerson} 
+          setViewGroup={setViewGroup}
+        />
         <Autocomplete 
           setSenatorId={setSenatorId} 
           viewPerson={viewPerson} 
           viewGroup={viewGroup} 
           allSenateVoteYaml={data.allSenateVoteYaml}
         />
+        <MockBarchart setPopupState={setPopupState}/>
       </Layout>
     </div>
   )
