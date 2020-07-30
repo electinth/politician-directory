bd5d4038572e564406ee0c7eb918ff531c790f59
Sophon <sophonjampasornklin@gmail.com>
2020-07-30 14:53:00 +0700

    Bar Chart D3

--- src/components/page2/barChart.js
+++ src/components/page2/barChart.js
@@ -23,7 +23,7 @@ function DrawChart({
   const margin = { top: 50, right: 50, bottom: 0, left: 50 },
     height = is_On ? 300 : height_svg,
     width = w - margin.right - margin.left
-
+  // console.log(data,"data")
   useEffect(() => {
     const chart = d3
       .select(ref.current)
@@ -31,7 +31,7 @@ function DrawChart({
       .attr("width", width + margin.left + margin.right)
       .attr("height", height)
   }, [])
-
+  //sort
   useEffect(() => {
     if (is_starter_bars) {
       d3.selectAll("g").remove()
@@ -40,6 +40,10 @@ function DrawChart({
   }, [data])
 
   useEffect(() => {
+    if (is_starter_bars) {
+      d3.selectAll("g").remove()
+    }
+
     if (is_all) {
       d3.selectAll("g").remove()
       if (is_On) {
@@ -49,135 +53,148 @@ function DrawChart({
       }
       d3.select("svg").attr("height", height)
     } else {
-      d3.selectAll(".chart")
-        .attr("height", height)
-        .style("overflow-y", "visible")
-      console.log(is_On, "is_On")
+      if (is_On) {
+        d3.selectAll(".chart").attr("height", height)
+      } else {
+        d3.selectAll(".chart")
+          .attr("height", height)
+          .style("overflow-y", "visible")
+      }
+      d3.selectAll("svg").attr("height", height)
     }
-
     draw_bar()
   }, [is_On])
 
   function draw_bar() {
     const chart = d3.select(ref.current)
-    let series = d3.stack().keys(types.slice(1))(data)
-    const color = d3
-      .scaleOrdinal()
-      .domain(series.map(d => d.key))
-      .range(color_bars)
-      .unknown("#ccc")
 
-    let ids = data.map(d => d.id)
-    let y = d3
-      .scaleBand()
-      .domain(ids)
-      .range([0, height])
-      .padding(0.2)
+    let stack = d3
+      .stack()
+      .keys(types)
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone)
 
-    let x = d3
-      .scaleLinear()
-      .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
-      .range([0, search_id ? width - 300 : width])
+    // let series = d3.stack().keys(types.slice(1))(data)
+    // const series = stack(filtered_data)
+    // console.log(series)
 
-    var yAxis = g =>
-      g
-        .attr("transform", `translate(${0}, 0)`)
-        .attr("className", "yAxis")
-        .call(d3.axisLeft(y).ticks(null, "s"))
-        .call(g => g.selectAll(".domain").remove())
-        .call(g => g.selectAll("line").remove())
+    console.log(data)
 
-    const g = chart
-      .append("g")
-      .attr("transform", `translate(${margin.left}, 0)`)
-      .attr("className", "charts")
+    // const color = d3
+    //   .scaleOrdinal()
+    //   .domain(series.map(d => d.key))
+    //   .range(color_bars)
+    //   .unknown("#ccc")
 
-    const tiny = chart
-      .append("g")
-      .attr("transform", `translate(${margin.left}, 0)`)
-      .attr("className", "charts")
+    // let ids = data.map(d => d.id)
+    // let y = d3
+    //   .scaleBand()
+    //   .domain(ids)
+    //   .range([0, height])
+    //   .padding(0.2)
 
-    const rects = g
-      .selectAll("g")
-      .data(series)
-      .enter()
-      .append("g")
-      .attr("fill", d => color(d.key))
-    console.log(rects)
+    // let x = d3
+    //   .scaleLinear()
+    //   .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+    //   .range([0, search_id ? width - 300 : width])
 
-    const tiny_rects = tiny
-      .selectAll("g")
-      .data(series)
-      .enter()
-      .append("g")
-      .attr("fill", d => color(d.key))
-    console.log(tiny_rects)
+    // var yAxis = g =>
+    //   g
+    //     .attr("transform", `translate(${0}, 0)`)
+    //     .attr("className", "yAxis")
+    //     .call(d3.axisLeft(y).ticks(null, "s"))
+    //     .call(g => g.selectAll(".domain").remove())
+    //     .call(g => g.selectAll("line").remove())
 
-    const mouseover = d => {
-      d3.selectAll(".rect" + d.data.id).style("stroke", "black")
-    }
-    const mouseout = d => {
-      d3.selectAll(".rect" + d.data.id).style("stroke", "none")
-    }
-    const onClick = d => {
-      d3.selectAll(".rect" + d.data.id).style("stroke", "black")
-      console.log(d.data.id)
-    }
-    rects
-      .selectAll("rect")
-      .data(d => d)
-      .join(enter =>
-        enter
-          .append("rect")
-          .attr("x", d => x(d[0]))
-          .attr("y", d => y(d.data.id))
-          .attr("width", d =>
-            search_id ? width - x(d[0]) - 300 : width - x(d[0])
-          )
-          .attr("height", y.bandwidth())
-          .attr("class", d => "rect" + d.data.id)
-          .on("mouseover", is_all ? mouseover : "")
-          .on("mouseout", is_all ? mouseout : "")
-          .on("click", is_all ? onClick : "")
-          .attr("transform", `translate(${search_id ? 300 : 0}, 0)`)
-      )
+    // const g = chart
+    //   .append("g")
+    //   .attr("transform", `translate(${margin.left}, 0)`)
+    //   .attr("className", "charts")
 
-    if (search_id) {
-      tiny_rects
-        .selectAll("rect")
-        .data(d => d)
-        .join(enter =>
-          enter
-            .append("rect")
-            .attr("x", d => x(d[0]))
-            .attr("y", d => y(d.data.id))
-            .attr("width", d => (1 / 250) * width)
-            .attr("height", y.bandwidth())
-            .attr("class", d => "rect" + d.data.id)
-            .on("mouseover", is_all ? mouseover : "")
-            .on("mouseout", is_all ? mouseout : "")
-            .on("click", is_all ? onClick : "")
-            .attr("transform", `translate(${search_id ? 150 : 0}, 0)`)
-        )
-    }
+    // const tiny = chart
+    //   .append("g")
+    //   .attr("transform", `translate(${margin.left}, 0)`)
+    //   .attr("className", "charts")
 
-    if (is_yAxis) {
-      if (!is_On) {
-        g.append("g").call(yAxis)
-      }
-    }
-    if (is_On) {
-      g.append("text")
-        .attr("x", -30)
-        .attr("y", 15)
-        .style("font-size", "8px")
-        .text(data[0].id)
-      g.append("text")
-        .attr("x", -30)
-        .attr("y", height - 3)
-        .style("font-size", "8px")
-        .text(data[data.length - 1].id)
-    }
+    // const rects = g
+    //   .selectAll("g")
+    //   .data(series)
+    //   .enter()
+    //   .append("g")
+    //   .attr("fill", d => color(d.key))
+
+    // const tiny_rects = tiny
+    //   .selectAll("g")
+    //   .data(series)
+    //   .enter()
+    //   .append("g")
+    //   .attr("fill", d => color(d.key))
+
+    // const mouseover = d => {
+    //   d3.selectAll(".rect" + d.data.id).style("stroke", "black")
+    // }
+    // const mouseout = d => {
+    //   d3.selectAll(".rect" + d.data.id).style("stroke", "none")
+    // }
+    // const onClick = d => {
+    //   d3.selectAll(".rect" + d.data.id).style("stroke", "black")
+    //   console.log(d.data.id)
+    // }
+    // rects
+    //   .selectAll("rect")
+    //   .data(d => d)
+    //   .join(enter =>
+    //     enter
+    //       .append("rect")
+    //       .attr("x", d => x(d[0]))
+    //       .attr("y", d => y(d.data.id))
+    //       .attr("width", d =>
+    //         search_id ? width - x(d[0]) - 300 : width - x(d[0])
+    //       )
+    //       .attr("height", y.bandwidth())
+    //       .attr("class", d => "rect" + d.data.id)
+    //       .on("mouseover", is_all ? mouseover : "")
+    //       .on("mouseout", is_all ? mouseout : "")
+    //       .on("click", is_all ? onClick : "")
+    //       .attr("transform", `translate(${search_id ? 300 : 0}, 0)`)
+    //   )
+
+    // if (search_id) {
+    //   tiny_rects
+    //     .selectAll("rect")
+    //     .data(d => d)
+    //     .join(enter =>
+    //       enter
+    //         .append("rect")
+    //         .attr("x", d => x(d[0]))
+    //         .attr("y", d => y(d.data.id))
+    //         .attr("width", d => (1 / 250) * width)
+    //         .attr("height", y.bandwidth())
+    //         .attr("class", d => "rect" + d.data.id)
+    //         .on("mouseover", is_all ? mouseover : "")
+    //         .on("mouseout", is_all ? mouseout : "")
+    //         .on("click", is_all ? onClick : "")
+    //         .attr("transform", `translate(${search_id ? 150 : 0}, 0)`)
+    //     )
+    // }
+
+    // if (is_yAxis) {
+    //   if (!is_On) {
+    //     g.append("g").call(yAxis)
+    //   }
+    //   if (is_On) {
+    //     g.append("text")
+    //       .attr("x", -30)
+    //       .attr("y", 15)
+    //       .style("font-size", "8px")
+    //       .text(data[0].id)
+    //     g.append("text")
+    //       .attr("x", -30)
+    //       .attr("y", height - 3)
+    //       .style("font-size", "8px")
+    //       .text(data[data.length - 1].id)
+    //   }
+    // }
   }
   return (
     <div className="chart" css={cssHightChart}>
--- src/pages/senatePage2.js
+++ src/pages/senatePage2.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react"
 import _ from "lodash"
-
+import { graphql, useStaticQuery } from "gatsby"
 import DropDown from "../components/page2/dropdown"
 import BarChart from "../components/page2/barChart"
 import ToggleSwitch from "../components/page2/toggleSwitch"
 
-import data250 from "../contents/all250.yaml"
+// import data250 from "../contents/all250.yaml"
 
 const cssGroupChart = {
   height: "300px",
   overflowY: "scroll",
+  marginTop: "50px",
+}
+const cssGroupChartAll = {
+  height: "300px",
+  overflowY: "hidden",
   marginTop: "50px",
 }
 const cssColumnChart = {
@@ -17,9 +22,13 @@ const cssColumnChart = {
 }
 
 const Page2 = props => {
-  const [data, setData] = useState(data250)
+  // console.log(props.count_all_senate,'count_all_senate')
+  // const [data, setData] = useState(data250)
+  const [count_all_senate, setCount_all_senate] = useState(
+    props.count_all_senate
+  )
   const [search_id, setSearch_id] = useState("")
-  const [types, setTypes] = useState(props.types_250)
+  const [types, setTypes] = useState(props.types)
   const [is_yAxis, setYAxis] = useState(props.is_yAxis)
   const [width, setWidth] = useState(props.width)
   const [is_senate, setIs_senate] = useState(true)
@@ -28,7 +37,7 @@ const Page2 = props => {
   const [is_showGroup, setShowGroup] = useState(false)
   const [currentFilter, setCurrentFilter] = useState(props.choices)
   const [is_On, setIsOn] = useState(false)
-  const [height_svg, setHeightSvg] = useState(data250.length * 30)
+  const [height_svg, setHeightSvg] = useState(count_all_senate.length * 30)
   const [is_all, setIs_all] = useState(true)
 
   const formatTypes = type => {
@@ -54,9 +63,9 @@ const Page2 = props => {
     }
     let currentFilter = { ...currentFilter }
     formatTypes(filter)
-    let data_of_motion = [...data250]
+    let data_of_motion = [...count_all_senate]
     data_of_motion.sort(sort_by_data)
-    setData(data_of_motion)
+    setCount_all_senate(data_of_motion)
     setCurrentFilter(currentFilter)
   }
 
@@ -64,6 +73,7 @@ const Page2 = props => {
     setShowAll(true)
     setShowGroup(false)
     setIs_all(true)
+    setIsOn(true)
   }
   const ShowGroup = () => {
     setShowAll(false)
@@ -71,6 +81,7 @@ const Page2 = props => {
     setIs_all(false)
     setIsOn(false)
   }
+
   return (
     <div>
       Page 2<button onClick={showAll}>show all</button>
@@ -86,7 +97,7 @@ const Page2 = props => {
           />
           <ToggleSwitch is_On={is_On} handleToggle={() => setIsOn(!is_On)} />
           <BarChart
-            data={data}
+            data={count_all_senate}
             types={types}
             w={width}
             is_yAxis={is_yAxis}
@@ -99,7 +110,8 @@ const Page2 = props => {
           />
         </div>
       ) : (
-        <div>
+        {
+          /* <div>
           <div css={{ ...cssColumnChart, width: (32 / 250) * width }}>
             <DropDown
               choices={props.choices}
@@ -127,7 +139,11 @@ const Page2 = props => {
               colors={props.colors}
             />
           </div>
-          <div className="group_chart" css={cssGroupChart}>
+          <ToggleSwitch
+              is_On = {is_On}
+              handleToggle = {() => setIsOn(!is_On)}
+          />
+          <div className="group_chart" css={is_On ? cssGroupChartAll : cssGroupChart}>
             <div css={{ ...cssColumnChart, width: (32 / 250) * width }}>
               <BarChart
                 data={data}
@@ -138,6 +154,7 @@ const Page2 = props => {
                 is_starter_bars={is_starter_bars}
                 height_svg={height_svg}
                 is_On={is_On}
+                is_all={is_all}
               />
             </div>
             <div css={{ ...cssColumnChart, width: (88 / 250) * width }}>
@@ -148,6 +165,7 @@ const Page2 = props => {
                 color_bars={props.colors}
                 height_svg={height_svg}
                 is_On={is_On}
+                is_all={is_all}
               />
             </div>
             <div css={{ ...cssColumnChart, width: (130 / 250) * width }}>
@@ -158,23 +176,151 @@ const Page2 = props => {
                 color_bars={props.colors}
                 height_svg={height_svg}
                 is_On={is_On}
+                is_all={is_all}
               />
             </div>
-            {/* <ToggleSwitch
-              is_On = {is_On}
-              handleToggle = {() => setIsOn(!is_On)}
-            /> */}
           </div>
-        </div>
+        </div> */
+        }
       )}
     </div>
   )
 }
 
 export default ({ data }) => {
-  const types = ["id", "agree", "not_agree", "no_vote", "not_vote", "missing"]
+  const senate = useStaticQuery(
+    graphql`
+      query {
+        allSenateVoteYaml {
+          nodes {
+            id
+            votelog {
+              key
+              value
+            }
+          }
+        }
 
-  const types_250 = ["id", "1", "2", "3", "4", "5"]
+        allSenateVotelogYaml {
+          nodes {
+            id
+            vote_date
+          }
+        }
+        allPeopleYaml {
+          nodes {
+            id
+            senator_method
+          }
+        }
+      }
+    `
+  )
+
+  const people_votes = senate.allSenateVoteYaml.nodes
+  const votelogs = senate.allSenateVotelogYaml.nodes
+  const people_method = senate.allPeopleYaml.nodes
+
+  const voter_in_votelog = []
+  people_votes.forEach(p => {
+    p.votelog.forEach(l => {
+      const method = people_method.filter(m => m.id == p.id)
+      method.forEach(pm => {
+        voter_in_votelog.push({
+          ...l,
+          senator_id: p.id,
+          senator_method: pm.senator_method,
+        })
+      })
+    })
+  })
+  _.remove(voter_in_votelog, function(n) {
+    return n.value === "-"
+  })
+
+  const default_value = [
+    { "1": 0 },
+    { "2": 0 },
+    { "3": 0 },
+    { "4": 0 },
+    { "5": 0 },
+  ]
+
+  function merge_default(default_value, group_by_value) {
+    const merge = [...default_value, ...group_by_value]
+    const result = {}
+
+    merge.forEach(basket => {
+      for (let [key, value] of Object.entries(basket)) {
+        if (result[key]) {
+          result[key] += value
+        } else {
+          result[key] = value
+        }
+      }
+    })
+    return result
+  }
+
+  const arr_votelog = votelogs.map(v => {
+    const voter = voter_in_votelog.filter(p => p.key == v.id)
+    return { ...v, voter }
+  })
+
+  const count_all_senate = []
+  arr_votelog.forEach(s => {
+    let group_by_value = _(s.voter)
+      .groupBy("value")
+      .map(function(votes, value) {
+        const zip = _.zipObject([value], [votes.length])
+        return { ...zip }
+      })
+      .value()
+
+    group_by_value = merge_default(default_value, group_by_value)
+    group_by_value = { ...group_by_value, id: s.id }
+    count_all_senate.push({ ...s, group_by_value })
+  })
+  console.log(count_all_senate)
+
+  const count_groupBy = []
+  arr_votelog.forEach(s => {
+    const by_position = s.voter.filter(m => m.senator_method == "โดยตำแหน่ง")
+    const group_by_position = _(by_position)
+      .groupBy("value")
+      .map(function(votes, value) {
+        return { value: value, count: votes.length }
+      })
+      .value()
+
+    const by_government = s.voter.filter(
+      m => m.senator_method == "เลือกโดย คสช."
+    )
+    const group_by_government = _(by_government)
+      .groupBy("value")
+      .map(function(votes, value) {
+        return { value: value, count: votes.length }
+      })
+      .value()
+
+    const by_youeSelf = s.voter.filter(m => m.senator_method == "เลือกกันเอง")
+    const group_by_youeSelf = _(by_youeSelf)
+      .groupBy("value")
+      .map(function(votes, value) {
+        return { value: value, count: votes.length }
+      })
+      .value()
+
+    const group_of_voters = {
+      group_by_position,
+      group_by_government,
+      group_by_youeSelf,
+    }
+    count_groupBy.push({ ...s, group_of_voters })
+  })
+  // console.log(count_groupBy)
+
+  const types = ["id", "1", "2", "3", "4", "5"]
   const is_yAxis = true
 
   const width = window.innerWidth
@@ -190,9 +336,8 @@ export default ({ data }) => {
 
   return (
     <Page2
-      data={data}
+      count_all_senate={count_all_senate}
       types={types}
-      types_250={types_250}
       width={width}
       is_yAxis={is_yAxis}
       choices={choices}
