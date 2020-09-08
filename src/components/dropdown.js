ae41a7fa55815220a80f3fd01cad64a721ff5c07
Naanong (Key) <naanong@punchup.world>
2020-09-08 13:33:34 +0700

    Delete global in dropdown component

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -2,6 +2,143 @@ import React, { Component } from "react"
 import { css, Global } from "@emotion/core"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+
+const cssDropdownDeafult = {
+  ".arrow-down": {
+    marginTop: "5px",
+    width: "0",
+    height: "0",
+    borderLeft: "5px solid transparent",
+    borderRight: "5px solid transparent",
+    borderTop: "10px solid var(--cl-black);",
+    float: "right",
+    marginLeft: "1rem",
+  },
+  ".current-filter-list": {
+    textAlign: "center",
+    listStyle: "none",
+    button: {
+      textAlign: "left",
+      background: "var(--cl-pink)",
+      border: "1px var(--cl-black) solid",
+      cursor: "pointer",
+      minWidth: "165px",
+      height: "40px",
+      margin: "1px 7.5px",
+      "&:focus": {
+        outline: "none",
+      },
+    },
+    li: {
+      display: "inline-block",
+      position: "relative;",
+    },
+  },
+  ".currentFilter": {
+    borderRadius: "7.5px",
+  },
+  ".menuItems": {
+    position: "absolute",
+    zIndex: "1",
+    border: "1px solid var(--cl-black)",
+    minWidth: "165px",
+    margin: "1px 7.5px",
+    button: {
+      display: "block",
+      border: "none",
+      backgroundColor: "white",
+      margin: "0px",
+      width: "100%",
+      "&:hover": {
+        backgroundColor: "var(--cl-pink)",
+      },
+    },
+  },
+}
+
+const cssDropdownSortBarchart = {
+  ".arrow-down": {
+    marginTop: "3px",
+    width: "0",
+    height: "0",
+    borderLeft: "6px solid transparent",
+    borderRight: "6px solid transparent",
+    borderTop: " 8px solid #aeaeae",
+    float: "right",
+    marginLeft: "1rem",
+  },
+  ".current-filter-list": {
+    textAlign: "center",
+    listStyle: "none",
+    position: "absolute",
+    button: {
+      textAlign: "left",
+      background: "white",
+      border: "1px solid #e0e0e0",
+      cursor: "pointer",
+      minWidth: "50px",
+      height: "31px",
+      margin: "1px 0",
+      "&:focus": {
+        outline: "none",
+        border: "1px solid #000000",
+        boxSizing: "border-box",
+        borderRadius: "5px",
+      },
+    },
+    li: {
+      display: "inline-block",
+      position: "relative;",
+    },
+  },
+  ".currentFilter": {
+    borderRadius: "7.5px",
+    margin: "0 0 0 5rem",
+    "&:hover": {
+      backgroundColor: "#eef090",
+    },
+  },
+  ".menuItems": {
+    position: "absolute",
+    zIndex: "1",
+    boxShadow:
+      "0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)",
+    minWidth: "185px",
+    margin: "1px 0",
+    button: {
+      display: "block",
+      border: "none",
+      backgroundColor: "white",
+      margin: "0px",
+      width: "100%",
+      "&:hover": {
+        backgroundColor: "#eef090",
+      },
+    },
+  },
+  ".bulletChoice , .clock_image": {
+    content: "''",
+    display: "inline-block",
+    width: "15px",
+    height: "15px",
+    borderRadius: "7.5px",
+    marginLeft: "20px",
+  },
+  ".cssChoice , .clock_text": {
+    display: "inline-block",
+    position: "absolute",
+    margin: "-2px 0 0 15px",
+  },
+  ".clock_image": {
+    marginTop: "5px",
+  },
+  ".clock_text": {
+    marginTop: "3px",
+  },
+}
+
+const cssDropdownSenatorType = {}
+
 class DropDown extends Component {
   container = React.createRef()
   state = {
@@ -27,53 +164,66 @@ class DropDown extends Component {
   }
   render() {
     return (
-      <li ref={this.container}>
-        <button
-          onClick={() => this.setState({ show: !this.state.show })}
-          className="currentFilter"
-        >
-          {this.state.is_senate ? (
-            <Img fixed={this.props.clock_image} />
-          ) : (
-            this.props.currentFilter[this.props.filter]
-          )}
-          <span>
-            <i
-              className="arrow-down"
-              style={this.state.show ? { transform: "rotate(180deg)" } : {}}
-            ></i>
-          </span>
-        </button>
-        {this.state.show ? (
-          <div className="menuItems">
-            <button onClick={e => this.handleFilter(e, this.props.filter)}>
+      <div
+        css={
+          this.state.is_senate ? cssDropdownSortBarchart : cssDropdownDeafult
+        }
+      >
+        <ul className="current-filter-list" css={{ marginLeft: 0 }}>
+          <li ref={this.container} className="css-dropdown">
+            <button
+              onClick={() => this.setState({ show: !this.state.show })}
+              className="currentFilter"
+            >
               {this.state.is_senate ? (
-                <div>
-                  <Img fixed={this.props.clock_image} className="clock_image" />
-                  <div className="clock_text">เวลาล่าสุด</div>
-                </div>
+                <Img fixed={this.props.clock_image} />
               ) : (
-                this.props.choices.default
+                this.props.currentFilter[this.props.filter]
               )}
+              <span>
+                <i
+                  className="arrow-down"
+                  style={this.state.show ? { transform: "rotate(180deg)" } : {}}
+                ></i>
+              </span>
             </button>
-            {this.props.choices.others.map((choice, i) => (
-              <button onClick={e => this.handleFilter(e, this.props.filter)}>
-                {this.state.is_senate ? (
-                  <>
-                    <div
-                      className="bulletChoice"
-                      style={{ backgroundColor: this.props.colors[i] }}
-                    />
-                    <div className="cssChoice">{choice}</div>
-                  </>
-                ) : (
-                  choice
-                )}
-              </button>
-            ))}
-          </div>
-        ) : null}
-      </li>
+            {this.state.show ? (
+              <div className="menuItems">
+                <button onClick={e => this.handleFilter(e, this.props.filter)}>
+                  {this.state.is_senate ? (
+                    <div>
+                      <Img
+                        fixed={this.props.clock_image}
+                        className="clock_image"
+                      />
+                      <div className="clock_text">เวลาล่าสุด</div>
+                    </div>
+                  ) : (
+                    this.props.choices.default
+                  )}
+                </button>
+                {this.props.choices.others.map((choice, i) => (
+                  <button
+                    onClick={e => this.handleFilter(e, this.props.filter)}
+                  >
+                    {this.state.is_senate ? (
+                      <>
+                        <div
+                          className="bulletChoice"
+                          style={{ backgroundColor: this.props.colors[i] }}
+                        />
+                        <div className="cssChoice">{choice}</div>
+                      </>
+                    ) : (
+                      choice
+                    )}
+                  </button>
+                ))}
+              </div>
+            ) : null}
+          </li>
+        </ul>
+      </div>
     )
   }
 }
@@ -96,148 +246,7 @@ export default ({
     }
   `)
   return (
-    <ul className="current-filter-list" css={{ marginLeft: 0 }}>
-      {is_senate ? (
-        <Global
-          styles={css`
-            .arrow-down {
-              margin-top: 3px;
-              width: 0;
-              height: 0;
-              border-left: 6px solid transparent;
-              border-right: 6px solid transparent;
-              border-top: 8px solid #aeaeae;
-              float: right;
-              margin-left: 1rem;
-            }
-            .current-filter-list {
-              text-align: center;
-              list-style: none;
-              position: absolute;
-              button {
-                text-align: left;
-                background: white;
-                border: 1px solid #e0e0e0;
-                cursor: pointer;
-                min-width: 50px;
-                height: 31px;
-                margin: 1px 7.5px;
-              }
-              .currentFilter {
-                border-radius: 7.5px;
-                margin: 0 0 0 5rem;
-              }
-              .currentFilter:hover {
-                background-color: #eef090;
-              }
-              button:focus {
-                outline: none;
-                border: 1px solid #000000;
-                box-sizing: border-box;
-                border-radius: 5px;
-              }
-              li {
-                display: inline-block;
-                position: relative;
-              }
-            }
-            .menuItems {
-              position: absolute;
-              z-index: 1;
-              box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2),
-                0 6px 10px 0 rgba(0, 0, 0, 0.19);
-              min-width: 185px;
-              margin: 1px 50px;
-              button {
-                display: block;
-                border: none;
-                background-color: white;
-                margin: 0px;
-                width: 100%;
-              }
-              button:hover {
-                background-color: #eef090;
-              }
-            }
-            .bulletChoice,
-            .clock_image {
-              content: "";
-              display: inline-block;
-              width: 15px;
-              height: 15px;
-              border-radius: 7.5px;
-              margin-left: 20px;
-            }
-            .cssChoice,
-            .clock_text {
-              display: inline-block;
-              position: absolute;
-              margin: -2px 0 0 15px;
-            }
-            .clock_image {
-              margin-top: 5px;
-            }
-            .clock_text {
-              margin-top: 3px;
-            }
-          `}
-        />
-      ) : (
-        <Global
-          styles={css`
-            .arrow-down {
-              margin-top: 5px;
-              width: 0;
-              height: 0;
-              border-left: 5px solid transparent;
-              border-right: 5px solid transparent;
-              border-top: 10px solid var(--cl-black);
-              float: right;
-              margin-left: 1rem;
-            }
-            .current-filter-list {
-              text-align: center;
-              list-style: none;
-              button {
-                text-align: left;
-                background: var(--cl-pink);
-                border: 1px var(--cl-black) solid;
-                cursor: pointer;
-                min-width: 165px;
-                height: 40px;
-                margin: 1px 7.5px;
-              }
-              .currentFilter {
-                border-radius: 7.5px;
-              }
-              button:focus {
-                outline: none;
-              }
-              li {
-                display: inline-block;
-                position: relative;
-              }
-            }
-            .menuItems {
-              position: absolute;
-              z-index: 1;
-              border: 1px solid var(--cl-black);
-              min-width: 165px;
-              margin: 1px 7.5px;
-              button {
-                display: block;
-                border: none;
-                background-color: white;
-                margin: 0px;
-                width: 100%;
-              }
-              button:hover {
-                background-color: var(--cl-pink);
-              }
-            }
-          `}
-        />
-      )}
+    <div>
       {Object.entries(choices).map(([filter, choices], key) => (
         <DropDown
           currentFilter={currentFilter}
@@ -250,6 +259,6 @@ export default ({
           clock_image={data.clock.childImageSharp.fixed}
         />
       ))}
-    </ul>
+    </div>
   )
 }
