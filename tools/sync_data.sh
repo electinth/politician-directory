5837c7b67ef9914c8beb910f7edc4d190650d79f
Rapee Suveeranont <iiieyes@gmail.com>
2021-03-22 10:47:40 +0700

    Prepare Preview workflows for Github Actions

--- tools/sync_data.sh
+++ tools/sync_data.sh
@@ -1,5 +1,5 @@
-# $ cd tools/
-# $ ./sync_google_sheets.sh
+# Sync data from Google Sheets to YAML files
+# $ tools/sync_data.sh
 
 # Clean
 rm -rf build.log tmp
@@ -9,19 +9,19 @@ mkdir -p tmp/
 echo "people"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1739107794&single=true&output=csv' \
   | sed '1d' > tmp/people.csv
-node -r esm csv2yaml tmp/people.csv >> build.log
+node -r esm tools/csv2yaml tmp/people.csv >> build.log
 
 # Party
 echo "party"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1527118532&single=true&output=csv' \
   | sed '1,3d' > tmp/party.csv
-node -r esm csv2yaml tmp/party.csv >> build.log
+node -r esm tools/csv2yaml tmp/party.csv >> build.log
 
 # Votelog
 echo "votelog"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=1873669969&single=true&output=csv' \
   | sed '1d' > tmp/votelog.csv
-node -r esm csv2yaml tmp/votelog.csv >> build.log
+node -r esm tools/csv2yaml tmp/votelog.csv >> build.log
 
 # PeopleVotelog
 echo "people_vote"
@@ -29,31 +29,31 @@ curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ
   > tmp/people_vote.bk.csv
 DEL_LINE=$(($(grep -n '^id,' tmp/people_vote.bk.csv | cut -d: -f 1)-1))
 cat tmp/people_vote.bk.csv | sed "1,${DEL_LINE}d" > tmp/people_vote.csv
-node -r esm csv2yaml tmp/people_vote.csv >> build.log
+node -r esm tools/csv2yaml tmp/people_vote.csv >> build.log
 
 # Motion
 echo "motion"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=1593490955&single=true&output=csv' \
   | sed '1d' > tmp/motion.csv
-node -r esm csv2yaml tmp/motion.csv >> build.log
+node -r esm tools/csv2yaml tmp/motion.csv >> build.log
 
 # Motion Categories
 echo "motion_cat"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXuBDf_n-vm78RYeLJKTmeIY-zwXg43zuFOBqd3IFYWskzkgPnVQY5BA9rkbX6NiXpUWrdkwfqL7-j/pub?gid=877287671&single=true&output=csv' \
   > tmp/motion_cat.csv
-node -r esm csv2yaml tmp/motion_cat.csv >> build.log
+node -r esm tools/csv2yaml tmp/motion_cat.csv >> build.log
 
 # Translations
 echo "translation"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=348578468&single=true&output=csv' \
   | sed '1d' > tmp/translation.csv
-node -r esm csv2yaml tmp/translation.csv >> build.log
+node -r esm tools/csv2yaml tmp/translation.csv >> build.log
 
 # Senate Votelog
 echo "senate_votelog"
 curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ4pPGdudulFq-uoek_ATkLOTi7AI9qMH7FhlwONC4N-TVLjokombSYfDu6G5/pub?gid=804641409&single=true&output=csv' \
   | sed '1d' > tmp/senate_votelog.csv
-node -r esm csv2yaml tmp/senate_votelog.csv >> build.log
+node -r esm tools/csv2yaml tmp/senate_votelog.csv >> build.log
 
 # Senate Vote
 echo "senate_vote"
@@ -61,8 +61,8 @@ curl -s -L 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyM5D8LOVvjGICiUdbQ
   > tmp/senate_vote.bk.csv
 DEL_LINE=$(($(grep -n '^id,' tmp/senate_vote.bk.csv | cut -d: -f 1)-1))
 cat tmp/senate_vote.bk.csv | sed "1,${DEL_LINE}d" > tmp/senate_vote.csv
-node -r esm csv2yaml tmp/senate_vote.csv >> build.log
+node -r esm tools/csv2yaml tmp/senate_vote.csv >> build.log
 
 # Move
-mv tmp/*.yaml ../src/contents
+mv tmp/*.yaml ./src/contents
 rm -rf tmp/
