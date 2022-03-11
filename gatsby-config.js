e0568a938f7950e72d18385a12a08426011b1583
Th1nkK1D <witheep@gmail.com>
2022-03-11 11:10:24 +0200

    Disable offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ const config = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-plausible`,
       options: {
